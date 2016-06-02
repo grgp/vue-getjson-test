d81f2a14ad28cc3151b532bd776d8685269d5170
grgp <g.a.pitoy@gmail.com>
2016-06-02 10:05:53 +0700

    xmlhttprequest working with vue

---
+++ app.js
@@ -0,0 +1,27 @@
+var apiURL = 'http://139.59.248.88/'
+
+var chart = new Vue({
+
+	el: '#chart',
+  
+  data: { 
+		chdata: null
+	},
+  
+  created: function () {
+  	this.fetchData()
+  },
+  
+  methods: {
+  	fetchData: function () {
+    	var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', apiURL)
+      xhr.onload = function () {
+      	self.chdata = JSON.parse(xhr.responseText)
+      }
+      xhr.send()
+    }
+  }
+  
+})
